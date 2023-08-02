5252863049340b8a8849e6518449303a77520a8c
liv553 <livia.balmeida@ufpe.br>
2023-08-02 19:27:58 -0300

    interface

---
+++ src/model/entities/Vehicle.java
@@ -0,0 +1,14 @@
+package model.entities;
+
+public class Vehicle {
+    private String name;
+
+    public Vehicle() {
+    }
+
+    public Vehicle(String name) {
+        this.name = name;
+    }
+
+
+}
